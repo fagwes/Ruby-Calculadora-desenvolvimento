198ae469fe84491e3779c34a41d7a5a983f3196f
fagwes <109398136+fagwes@users.noreply.github.com>
2023-04-12 21:08:10 -0300

    Update Calculadora.rb

--- Calculadora.rb
+++ Calculadora.rb
@@ -1,27 +1,39 @@
-puts "Selecione a operação desejada:"
-puts "1 - Soma"
-puts "2 - Subtração"
-puts "3 - Multiplicação"
-puts "4 - Divisão"
+def calculadora
+  puts "Selecione a operação desejada:"
+  puts "1 - Soma"
+  puts "2 - Subtração"
+  puts "3 - Multiplicação"
+  puts "4 - Divisão"
 
-op = gets.chomp.to_i
-puts "Digite o primeiro número: "
-num1 = gets.chomp.to_f
-puts "Digite o segundo número: "
-num2 = gets.chomp.to_f
+  op = gets.chomp.to_i
+  puts "Digite o primeiro número: "
+  num1 = gets.chomp.to_i
+  puts "Digite o segundo número: "
+  num2 = gets.chomp.to_i
 
-if op == 1
-  puts "#{num1} + #{num2} = #{num1+num2}"
-elsif op == 2
-  puts "#{num1} - #{num2} = #{num1-num2}"
-elsif op == 3
-  puts "#{num1} * #{num2} = #{num1*num2}"
-elsif op == 4
-  if num2 == 0
-    puts "Não é possível dividir por zero!"
+  if op == 1
+    resultado = num1 + num2
+  elsif op == 2
+    resultado = num1 - num2
+  elsif op == 3
+    resultado = num1 * num2
+  elsif op == 4
+    if num2 == 0
+      puts "Não é possível dividir por zero!"
+      return
+    else
+      resultado = num1 / num2
+    end
   else
-    puts "#{num1} / #{num2} = #{num1/num2}"
+    puts "Opção inválida!"
+    return
   end
-else
-  puts "Opção inválida!"
+
+  if resultado % 2 == 0
+    puts "O resultado é #{resultado} (par)."
+  else
+    puts "O resultado é #{resultado} (ímpar)."
+  end
 end
+
+calculadora()
